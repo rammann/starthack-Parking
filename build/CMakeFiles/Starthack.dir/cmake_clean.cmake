4d460dab6b028373d8f5f11cd912390ffa0b7c5d
Ryan <rammann@student.ethz.ch>
2021-03-20 16:08:16 +0100

    alot of stuff

---
+++ build/CMakeFiles/Starthack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Starthack.dir/sbb.cpp.o"
+  "Starthack"
+  "Starthack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Starthack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
